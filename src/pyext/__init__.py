a3e19a49d1ec3a8f543e034e50391f97cf985be1
Benjamin Meyer <benjamin.meyer@fhnw.ch>
2019-03-04 10:25:51 +0100

    Add oLift

--- src/pyext/__init__.py
+++ src/pyext/__init__.py
@@ -1,7 +1,10 @@
 from pyext import collections
 from pyext.collections.none import _None
-from pyext.collections.option import T
+from pyext.collections.option import T, Option
 from pyext.collections.some import Some
+
+
+oLift = Option.lift
 
 
 def some(value: T) -> Some[T]:
