a9ddcb3fd4d65d2a77c2bd4f2db6be6a8e8b8c6b
Benjamin Meyer <benjamin.meyer@fhnw.ch>
2019-03-04 10:23:13 +0100

    Add lift helper function.

--- src/pyext/__init__.py
+++ src/pyext/__init__.py
@@ -1,10 +1,7 @@
-from typing import TypeVar
-
+from pyext import collections
 from pyext.collections.none import _None
+from pyext.collections.option import T
 from pyext.collections.some import Some
-
-
-T = TypeVar('T')
 
 
 def some(value: T) -> Some[T]:
--- src/pyext/collections/option.py
+++ src/pyext/collections/option.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import TypeVar, Generic, Callable
 
+from pyext import some, none
+
 T = TypeVar('T')
 Out = TypeVar('Out')
 
@@ -10,6 +12,10 @@ class Option(Generic[T], ABC):
 
     def __init__(self):
         pass
+
+    @staticmethod
+    def lift(e, noneValue=None):
+        return some(e) if e is not noneValue else none()
 
     @abstractmethod
     def map(self, f: Callable[[T], Out]) -> 'Option[T]':
